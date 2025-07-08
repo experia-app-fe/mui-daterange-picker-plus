f9cc37b43a7a8a4fb5eae555b52db26abb44226a
Bilal Abouzid <imbilalabouzid@gmail.com>
2025-07-08 14:28:12 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { PickerModal } from "./PickerModal";
+import { PickerBase } from "./PickerBase";
+import type { PickerBaseProps, PickerModalProps } from "./types";
+import type {
+  DateRange,
+  PickerProps,
+  DefinedRange,
+  Marker,
+  ModalCustomProps,
+  RangeSeparatorIconsProps,
+} from "./types/utils";
+
+export { PickerModal, PickerBase };
+
+export type {
+  Marker,
+  DateRange,
+  DefinedRange,
+  RangeSeparatorIconsProps,
+  ModalCustomProps,
+  PickerProps,
+  PickerModalProps,
+  PickerBaseProps,
+};
